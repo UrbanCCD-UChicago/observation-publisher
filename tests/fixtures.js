050a31a14bc433767a5dc6135085eabc63de1ea2
Manasi Thakkar <mat185@Pitt.edu>
2017-07-05 11:44:23 -0500

    add newline

--- tests/fixtures.js
+++ tests/fixtures.js
@@ -28,8 +28,8 @@ const beehiveRecords = [
 // except those that are malformed.
 
 const firehoseRows = [
-    'array_of_things_chicago,0000001e0610b9e7,2017-04-07 17:50:51,1,bmi160,"{""orient_y"":1,""orient_z"":-1,""accel_z"":30,""orient_x"":3,""accel_y"":981,""accel_x"":-10}"',
-    'array_of_things_chicago,0000001e0610b9e7,2017-04-07 17:50:51,2,tmp112,"{""temperature"":23.93}"'
+    'array_of_things_chicago,0000001e0610b9e7,2017-04-07 17:50:51,1,bmi160,"{""orient_y"":1,""orient_z"":-1,""accel_z"":30,""orient_x"":3,""accel_y"":981,""accel_x"":-10}"\n',
+    'array_of_things_chicago,0000001e0610b9e7,2017-04-07 17:50:51,2,tmp112,"{""temperature"":23.93}"\n'
 ]
 
 const redisRecords = [
@@ -47,7 +47,7 @@ const redisRecords = [
     },
     sensor: "bmi160",
     node: "0000001e0610b9e7"
-  }\n,
+  },
   {
     datetime: "2017-04-07T17:50:51",
     network: "array_of_things_chicago",
@@ -55,8 +55,7 @@ const redisRecords = [
     data: { temperature: 23.93},
     sensor: "tmp112",
     node: "0000001e0610b9e7"
-  }\n
-]
+  }];
 
 exports.beehiveRecords = beehiveRecords;
 exports.firehoseRows = firehoseRows;
